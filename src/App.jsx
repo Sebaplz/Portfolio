c0172a2554c3b2a569a646bc2b848a1db86ee772
Sebaplz <sebastian.h.neira@outlook.com>
2023-06-12 01:44:23 -0400

    Se agrega el modo claro

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { AiFillLinkedin, AiFillHtml5, AiFillGithub } from "react-icons/ai";
 import { DiReact, DiJsBadge, DiCss3 } from "react-icons/di";
 import { SiTailwindcss } from "react-icons/si";
 import { BsBootstrapFill, BsFillArrowUpCircleFill } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 import { motion } from "framer-motion";
 
 function App() {
@@ -15,13 +16,13 @@ function App() {
   return (
     <>
       <div className={darkMode ? "dark" : ""}>
-        <header className="p-5 bg-slate-800 max-h-[8vh] fixed w-full z-50">
+        <header className="p-5 dark:bg-slate-800 bg-slate-700 max-h-[8vh] fixed w-full z-50">
           <Navbar handleToggleDarkMode={toggleDarkMode} />
         </header>
 
-        <main className="dark:bg-gradient-to-t dark:from-[#1e293b] dark:to-[#0f172a] bg-slate-400 ">
-          <section className="min-h-screen" id="principal">
-            <div className="flex flex-col items-center pt-48 md:pt-28 lg:pt-44 lg:flex-row lg:justify-center">
+        <main className="dark:bg-gradient-to-t dark:from-[#1e293b] dark:to-[#0f172a] bg-slate-200">
+          <section className="min-h-[92vh]" id="principal">
+            <div className="flex flex-col items-center pt-48 md:pt-36 lg:pt-72 xl:pt-60 lg:flex-row lg:justify-center">
               <motion.div
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
@@ -30,12 +31,12 @@ function App() {
                   delay: 0.5,
                   ease: [0, 0.71, 0.2, 1.01],
                 }}
-                className="text-slate-950 dark:text-slate-50 px-4"
+                className="text-slate-800 dark:text-slate-50 px-4"
               >
-                <h1 className="text-5xl md:text-6xl lg:text-7xl">
+                <h1 className="text-4xl sm:text-5xl md:text-6xl 2xl:text-7xl font-bold">
                   Sebastián Neira
                 </h1>
-                <h2 className="text-2xl md:text-3xl lg:text-4xl">
+                <h2 className="text-2xl md:text-3xl lg:text-4xl text-center lg:text-left">
                   Full Stack Developer
                 </h2>
               </motion.div>
@@ -73,21 +74,28 @@ function App() {
               >
                 <a
                   href="#"
-                  className="text-slate-300 bg-[#087ea4] p-3 rounded-full font-semibold hover:text-slate-50 lg:text-xl"
+                  className="dark:text-slate-50 text-slate-800 bg-[#087ea4] p-3 rounded-full font-semibold lg:text-xl"
                 >
                   Descargar CV
                 </a>
               </motion.div>
               <div className="flex mt-5 justify-center items-center">
                 <AiFillLinkedin
-                  className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl"
+                  className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl"
                   onClick={() =>
                     (window.location.href =
                       "https://www.linkedin.com/in/sebastian-neira/")
                   }
                 />
+                <FiMail
+                  className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl"
+                  onClick={() =>
+                    (window.location.href =
+                      "mailto:sebastian.h.neira@outlook.com")
+                  }
+                />
                 <AiFillGithub
-                  className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl"
+                  className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl"
                   onClick={() =>
                     (window.location.href = "https://github.com/Sebaplz")
                   }
@@ -99,24 +107,32 @@ function App() {
             className="min-h-[30vh] pt-20 p-4 w-full md:w-3/4 xl:w-1/2 mx-auto"
             id="about"
           >
-            <h2 className="text-slate-50 text-center text-2xl md:text-3xl lg:text-4xl">
+            <h2 className="text-slate-800 dark:text-slate-50 text-center text-2xl md:text-3xl lg:text-4xl font-bold">
               Sobre Mí
             </h2>
-            <p className="text-slate-50 text-justify mt-4">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Similique iure quas alias deleniti ducimus saepe dolor nisi veniam
-              dolorum excepturi culpa ipsam eaque, perspiciatis nihil recusandae
-              minima. Sapiente error laboriosam laborum. Laboriosam architecto,
-              deserunt non perspiciatis tempore dolore ea temporibus.
+            <p className="text-slate-800 dark:text-slate-50 text-justify mt-4 font-medium">
+              Hola! Soy Sebastián Neira, Ingeniero en Informática, en busca de
+              oportunidades laborales que me permitan adquirir conocimientos y
+              seguir desarrollando mis habilidades como desarrollador. Soy un
+              apasionado de la tecnología, siempre busco aprender y aplicar
+              soluciones que puedan ayudar a las personas a mejorar su día a
+              día.
+            </p>
+            <p className="text-slate-800 dark:text-slate-50 text-center font-medium">
+              Mi lema es{" "}
+              <span className="italic">
+                &quot;El conocimiento así como la felicidad, es mejor cuando se
+                comparte&quot;.
+              </span>
             </p>
           </section>
           <section className="min-h-screen pt-20" id="proyectos">
-            <h2 className="text-slate-50 text-center text-2xl md:text-3xl mb-4">
+            <h2 className="text-slate-800 dark:text-slate-50 text-center text-2xl md:text-3xl mb-4 font-bold">
               Proyectos
             </h2>
             <div className="flex flex-col items-center gap-10 lg:flex-row md:px-10 xl:px-20">
               <article
-                className="border-2 border-slate-400 w-3/4 md:w-full transition ease-in-out delay-150 blur-sm hover:blur-none cursor-pointer rounded-lg"
+                className="border-2 border-slate-800 dark:border-slate-400 w-3/4 md:w-full transition ease-in-out delay-150 blur-[1px] hover:blur-none cursor-pointer rounded-lg"
                 onClick={() =>
                   (window.location.href =
                     "https://github.com/Sebaplz/weatherApi")
@@ -125,17 +141,17 @@ function App() {
                 <img
                   src="/WeatherApi.png"
                   alt="Weather Api"
-                  className="mx-auto xl:h-[40rem]"
+                  className="rounded-t-lg md:rounded-none mx-auto xl:h-[40rem]"
                 />
                 <div className="flex justify-center items-center p-2">
-                  <DiReact className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <DiJsBadge className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <AiFillHtml5 className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <SiTailwindcss className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
+                  <DiReact className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <DiJsBadge className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <AiFillHtml5 className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <SiTailwindcss className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
                 </div>
               </article>
               <article
-                className="border-2 border-slate-400 w-3/4 md:w-full transition ease-in-out delay-150 blur-sm hover:blur-none cursor-pointer rounded-lg"
+                className="border-2 border-slate-800 dark:border-slate-400 w-3/4 md:w-full cursor-pointer rounded-lg"
                 onClick={() =>
                   (window.location.href =
                     "https://github.com/Sebaplz/ip-address-tracker")
@@ -144,17 +160,17 @@ function App() {
                 <img
                   src="/IpTracker.png"
                   alt="Ip Tracker"
-                  className="mx-auto xl:h-[40rem]"
+                  className="rounded-t-lg md:rounded-none mx-auto xl:h-[40rem]"
                 />
                 <div className="flex justify-center items-center p-2">
-                  <DiReact className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <DiJsBadge className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <AiFillHtml5 className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <SiTailwindcss className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
+                  <DiReact className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <DiJsBadge className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <AiFillHtml5 className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <SiTailwindcss className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
                 </div>
               </article>
               <article
-                className="border-2 border-slate-400 w-3/4 md:w-full transition ease-in-out delay-150 blur-sm hover:blur-none cursor-pointer rounded-lg"
+                className="border-2 border-slate-800 dark:border-slate-400 w-3/4 md:w-full transition ease-in-out delay-150 blur-[1px] hover:blur-none cursor-pointer rounded-lg"
                 onClick={() =>
                   (window.location.href =
                     "https://github.com/Sebaplz/ip-address-tracker")
@@ -163,13 +179,13 @@ function App() {
                 <img
                   src="/DigiApi.png"
                   alt="Ip Tracker"
-                  className="mx-auto xl:h-[40rem]"
+                  className="rounded-t-lg md:rounded-none mx-auto xl:h-[40rem]"
                 />
                 <div className="flex justify-center items-center p-2">
-                  <DiJsBadge className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <AiFillHtml5 className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <DiCss3 className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
-                  <BsBootstrapFill className="cursor-pointer text-slate-400 hover:text-slate-50 text-5xl" />
+                  <DiJsBadge className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <AiFillHtml5 className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <DiCss3 className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
+                  <BsBootstrapFill className="cursor-pointer text-slate-700 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl" />
                 </div>
               </article>
             </div>
@@ -180,7 +196,7 @@ function App() {
             >
               <a
                 href="https://github.com/Sebaplz?tab=repositories"
-                className="text-slate-300 bg-[#087ea4] p-3 rounded-full font-sans font-semibold hover:text-slate-50 lg:text-xl"
+                className="text-slate-800 dark:text-slate-50 bg-[#087ea4] p-3 rounded-full font-sans font-semibold lg:text-xl"
               >
                 Ver todos los Proyectos 👀
               </a>
@@ -188,7 +204,7 @@ function App() {
           </section>
           <div className="flex justify-end p-5">
             <BsFillArrowUpCircleFill
-              className="animate-bounce cursor-pointer text-slate-400 hover:text-slate-50 text-5xl"
+              className="animate-bounce cursor-pointer text-slate-800 dark:text-slate-400 dark:hover:text-slate-50 text-5xl"
               onClick={() => (window.location.href = "#principal")}
             />
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,12 @@ function Navbar(props) {
   return (
     <>
       <nav className="md:flex md:justify-between md:items-center hidden">
-        <img src="/Logo.png" alt="Logo" className="w-8 h-8 hidden sm:block" />
+        <img
+          src="/Logo.png"
+          alt="Logo"
+          className="w-8 h-8 hidden sm:block cursor-pointer"
+          onClick={() => (window.location.href = "#principal")}
+        />
         <ul className="flex items-center">
           <li className="px-2">
             <a
@@ -29,7 +34,7 @@ function Navbar(props) {
               Proyectos
             </a>
           </li>
-          <li className="px-2">
+          <li className="pl-2 pr-10">
             <a
               href="#about"
               className="text-xl text-slate-400  hover:text-slate-50 hover:underline"
@@ -37,29 +42,29 @@ function Navbar(props) {
               Sobre mí
             </a>
           </li>
-          <li className="px-2">
-            <a
-              href="#"
-              className="text-xl text-slate-400 hover:text-slate-50 hover:underline"
-            >
-              Contacto
-            </a>
-          </li>
           <li className="px-2 flex items-center">
             <BsFillMoonStarsFill className="text-slate-400 dark:text-slate-50 text-xl" />
             <div className="switch mx-2" data-isOn={isOn} onClick={handleClick}>
-              <motion.div className="handle" layout transition={spring} />
+              <motion.div
+                className="handle dark:bg-slate-800"
+                layout
+                transition={spring}
+              />
             </div>
             <BsBrightnessHighFill className="text-xl dark:text-slate-400 text-slate-50" />
           </li>
         </ul>
       </nav>
       <nav className="flex justify-between items-center md:hidden">
-        <AiOutlineMenu className="text-slate-400 dark:text-slate-50 text-xl" />
+        <AiOutlineMenu className="text-slate-400 text-xl cursor-pointer hover:text-slate-50" />
         <div className="flex items-center px-2">
           <BsFillMoonStarsFill className="text-slate-400 dark:text-slate-50 text-xl" />
           <div className="switch mx-2" data-isOn={isOn} onClick={handleClick}>
-            <motion.div className="handle" layout transition={spring} />
+            <motion.div
+              className="handle dark:handle-dark"
+              layout
+              transition={spring}
+            />
           </div>
           <BsBrightnessHighFill className="text-xl dark:text-slate-400 text-slate-50" />
         </div>
